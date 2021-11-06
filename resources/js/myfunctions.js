f99800458a0435fd3985af50b0c26d21d90e2a1b
Ower A. Campos <owerion22@gmail.com>
2021-11-05 22:48:04 -0500

    cambios en app.js
    se realiza el cambio de rutas para el segundo servidor.

--- resources/js/myfunctions.js
+++ resources/js/myfunctions.js
@@ -66,8 +66,8 @@ const sinComa = event => {
 }
 
 function seeDirection($direction = '/home') {
-  // const initial = "colchildren";
-  const initial = "dreamhome";
+  const initial = "colchildren";
+  // const initial = "dreamhome";
   // const initial = ".";
   const administrativeHumans = `${initial}/administrative/humans`,
     administrativeServices = `${initial}/administrative/services`,
--- webpack.mix.js
+++ webpack.mix.js
@@ -16,6 +16,4 @@ mix.js('resources/js/app.js', 'public/js')
 
 mix.browserSync('http://kindergarden.test/');
 
-mix.version();
-
 mix.disableNotifications();

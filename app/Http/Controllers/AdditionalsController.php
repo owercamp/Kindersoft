ff7449678eb3aca57cafa56f7ce705150bac6b0c
Ower A. Campos <owerion22@gmail.com>
2022-02-13 13:10:46 -0500

    Cambios de facturación y adicionales

--- app/Http/Controllers/AdditionalsController.php
+++ app/Http/Controllers/AdditionalsController.php
@@ -44,10 +44,10 @@ class AdditionalsController extends Controller
   {
     try {
       /*
-                $request->addCourse;
-                $request->addStudent;
-                $request->addIdAttendant;
-                $request->addDate;
+      $request->addCourse;
+      $request->addStudent;
+      $request->addIdAttendant;
+      $request->addDate;
                 $request->addDescription;
                 $request->addAdmission;
                 $request->activeAdmission;
@@ -69,95 +69,95 @@ class AdditionalsController extends Controller
                 */
       // dd($request->all());
       $autorizatedValidate = Autorization::where('auDate', trim($request->addDate))
-        ->where('auStudent_id', trim($request->addStudent))
-        ->where('auCourse_id', trim($request->addCourse))
-        ->first();
-      $legalization = Legalization::select('legId')->where('legStudent_id', $request->addStudent)->first();
+      ->where('auStudent_id', trim($request->addStudent))
+      ->where('auCourse_id', trim($request->addCourse))
+      ->first();
+      $legalization = Legalization::select('legId')->where([['legStudent_id', $request->addStudent],['legStatus','ACTIVO']])->first();
       $autorized = '';
-
+      
       $separatedItems = explode('=', substr(trim($request->all_items), 0, -1));
-
+      
       for ($i = 0; $i < count($separatedItems); $i++) {
         $separatedIds = explode(':', $separatedItems[$i]);
         $autorized .= $separatedItems[$i] . '-';
         if ($separatedIds[0] == 'ADMISION') {
           $admission = Admission::find($separatedIds[1]);
-          Concept::create([
-            'conDate' => date('Y-m-d', strtotime($request->addDate . '+ 1 month')),
-            'conConcept' => 'ADMISION: ' . $admission->admConcept,
-            'conValue' => $admission->admValue,
-            'conLegalization_id' => $legalization->legId
-          ]);
+          $concept = new Concept;
+          $concept->conDate = date('Y-m-d', strtotime($request->addDate . '+ 1 month'));
+          $concept->conConcept = 'ADMISION: ' . $admission->admConcept;
+          $concept->conValue = $admission->admValue;
+          $concept->conLegalization_id = $legalization->legId;
+          $concept->save();
         } else if ($separatedIds[0] == 'JORNADA') {
           $journey = Journey::find($separatedIds[1]);
-          Concept::create([
-            'conDate' => date('Y-m-d', strtotime($request->addDate . '+ 1 month')),
-            'conConcept' => 'JORNADA: ' . $journey->jouJourney,
-            'conValue' => $journey->jouValue,
-            'conLegalization_id' => $legalization->legId
-          ]);
+          $concept = new Concept;
+          $concept->conDate = date('Y-m-d', strtotime($request->addDate . '+ 1 month'));
+          $concept->conConcept = 'JORNADA: ' . $journey->jouJourney;
+          $concept->conValue = $journey->jouValue;
+          $concept->conLegalization_id = $legalization->legId;
+          $concept->save();
         } else if ($separatedIds[0] == 'ALIMENTACION') {
           $feeding = Feeding::find($separatedIds[1]);
-          Concept::create([
-            'conDate' => date('Y-m-d', strtotime($request->addDate . '+ 1 month')),
-            'conConcept' => 'ALIMENTACION: ' . $feeding->feeConcept,
-            'conValue' => $feeding->feeValue,
-            'conLegalization_id' => $legalization->legId
-          ]);
+          $concept = new Concept;
+          $concept->conDate = date('Y-m-d', strtotime($request->addDate . '+ 1 month'));
+          $concept->conConcept = 'ALIMENTACION: ' . $feeding->feeConcept;
+          $concept->conValue = $feeding->feeValue;
+          $concept->conLegalization_id = $legalization->legId;
+          $concept->save();
         } else if ($separatedIds[0] == 'UNIFORME') {
           $uniform = Uniform::find($separatedIds[1]);
-          Concept::create([
-            'conDate' => date('Y-m-d', strtotime($request->addDate . '+ 1 month')),
-            'conConcept' => 'UNIFORME: ' . $uniform->uniConcept,
-            'conValue' => $uniform->uniValue,
-            'conLegalization_id' => $legalization->legId
-          ]);
+          $concept = new Concept;
+          $concept->conDate = date('Y-m-d', strtotime($request->addDate . '+ 1 month'));
+          $concept->conConcept = 'UNIFORME: ' . $uniform->uniConcept;
+          $concept->conValue = $uniform->uniValue;
+          $concept->conLegalization_id = $legalization->legId;
+          $concept->save();
         } else if ($separatedIds[0] == 'MATERIAL') {
           $supplie = Supplie::find($separatedIds[1]);
-          Concept::create([
-            'conDate' => date('Y-m-d', strtotime($request->addDate . '+ 1 month')),
-            'conConcept' => 'MATERIAL ESCOLAR: ' . $supplie->supConcept,
-            'conValue' => $supplie->supValue,
-            'conLegalization_id' => $legalization->legId
-          ]);
+          $concept = new Concept;
+          $concept->conDate = date('Y-m-d', strtotime($request->addDate . '+ 1 month'));
+          $concept->conConcept = 'MATERIAL ESCOLAR: ' . $supplie->supConcept;
+          $concept->conValue = $supplie->supValue;
+          $concept->conLegalization_id = $legalization->legId;
+          $concept->save();
         } else if ($separatedIds[0] == 'TIEMPO EXTRA') {
           $extratime = Extratime::find($separatedIds[1]);
-          Concept::create([
-            'conDate' => date('Y-m-d', strtotime($request->addDate . '+ 1 month')),
-            'conConcept' => 'TIEMPO EXTRA: ' . $extratime->extTConcept,
-            'conValue' => $extratime->extTValue,
-            'conLegalization_id' => $legalization->legId
-          ]);
+          $concept = new Concept;
+          $concept->conDate = date('Y-m-d', strtotime($request->addDate . '+ 1 month'));
+          $concept->conConcept = 'TIEMPO EXTRA: ' . $extratime->extTConcept;
+          $concept->conValue = $extratime->extTValue;
+          $concept->conLegalization_id = $legalization->legId;
+          $concept->save();
         } else if ($separatedIds[0] == 'EXTRACURRICULAR') {
           $extracurricular = Extracurricular::find($separatedIds[1]);
-          Concept::create([
-            'conDate' => date('Y-m-d', strtotime($request->addDate . '+ 1 month')),
-            'conConcept' => 'EXTRACURRICULAR: ' . $extracurricular->extConcept . ' ' . $extracurricular->extIntensity,
-            'conValue' => $extracurricular->extValue,
-            'conLegalization_id' => $legalization->legId
-          ]);
+          $concept = new Concept;
+          $concept->conDate = date('Y-m-d', strtotime($request->addDate . '+ 1 month'));
+          $concept->conConcept = 'EXTRACURRICULAR: ' . $extracurricular->extConcept . ' ' . $extracurricular->extIntensity;
+          $concept->conValue = $extracurricular->extValue;
+          $concept->conLegalization_id = $legalization->legId;
+          $concept->save();
         } else if ($separatedIds[0] == 'TRANSPORTE') {
           $transport = Transport::find($separatedIds[1]);
-          Concept::create([
-            'conDate' => date('Y-m-d', strtotime($request->addDate . '+ 1 month')),
-            'conConcept' => 'TRANSPORTE: ' . $transport->traConcept,
-            'conValue' => $transport->traValue,
-            'conLegalization_id' => $legalization->legId
-          ]);
+          $concept = new Concept;
+          $concept->conDate = date('Y-m-d', strtotime($request->addDate . '+ 1 month'));
+          $concept->conConcept = 'TRANSPORTE: ' . $transport->traConcept;
+          $concept->conValue = $transport->traValue;
+          $concept->conLegalization_id = $legalization->legId;
+          $concept->save();
         }
       }
       //QUITAR EL ULTIMO GUION (-)
       $autorizedAll = substr($autorized, 0, (strlen($autorized) - 1));
-
+      
       if ($autorizatedValidate == null) {
-        Autorization::create([
-          'auCourse_id' => trim($request->addCourse),
-          'auStudent_id' => trim($request->addStudent),
-          'auAttendant_id' => trim($request->addIdAttendant),
-          'auDate' => trim($request->addDate),
-          'auDescription' => trim($request->addDescription),
-          'auAutorized' => trim($autorizedAll),
-        ]);
+        $autorization = new Autorization;
+        $autorization->auCourse_id = trim($request->addCourse);
+        $autorization->auStudent_id = trim($request->addStudent);
+        $autorization->auAttendant_id = trim($request->addIdAttendant);
+        $autorization->auDate = trim($request->addDate);
+        $autorization->auDescription = trim($request->addDescription);
+        $autorization->auAutorized = trim($autorizedAll);
+        $autorization->save();
         return redirect()->route('additionals')->with('SuccessSaveAdditional', 'Autorización de las novedades adicionales, Se ha procesado correctamente');
       } else {
         $autorizatedValidate->auAttendant_id = $request->addIdAttendant;

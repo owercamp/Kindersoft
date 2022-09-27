3e80ae0b60b3cf5a3c4aeb24214da463a74dbcf9
Ower A. Campos <owerion22@gmail.com>
2022-09-26 21:07:50 -0500

    Actualización del modelo Documen roll

--- app/Models/DocumentEnroll.php
+++ app/Models/DocumentEnroll.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class DocumentEnroll extends Model
 {
-    protected $table = "documentsEnrollment";
+    protected $table = "documentsenrollment";
     protected $primaryKey = 'deId';
-    protected $fillable = [ 'deConcept','deRequired','deStatus','dePosition' ];
+    protected $fillable = ['deConcept', 'deRequired', 'deStatus', 'dePosition'];
     public $timestamps = false;
 }
